5fa15c6ccdf69abc4c3c1b6ade603d27c8a6d17b
Erik200031 <90761494+Erik200031@users.noreply.github.com>
2021-12-01 19:26:10 +0400

    Add files via upload

---
+++ Shashki.h
@@ -0,0 +1,31 @@
+#ifndef SHASHKI_H
+#define SHASHKI_H
+#include <iostream>
+#include <vector>
+#include "vandak.h"
+
+class Shashki{
+    public:
+    Shashki();
+    void start();
+    void enter_index();
+    bool check_at(int,int);
+    bool check_to(int,int);
+    void check_win();
+    private:
+    char vandak[8][8];
+    char figur;
+    bool state;
+    int at_index1;
+    int at_index2;
+    int to_index1;
+    int to_index2;
+    bool count;
+};
+
+
+
+
+
+
+#endif//SHASHKI_H
